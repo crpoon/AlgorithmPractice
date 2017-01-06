a05e7b87457bf092c572aa094b144837d10bb326
Christopher Poon <crpoon@gmail.com>
2017-01-05 20:54:37 -0800

    Initial Commit with #412 Fizz Buzz

---
+++ src/com/crpoon/leetcode/easy/FizzBuzz_412.java
@@ -0,0 +1,22 @@
+package com.crpoon.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FizzBuzz_412 {
+	public List<String> fizzBuzz(int n) {
+        List<String> result = new ArrayList<>();
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                result.add("FizzBuzz");
+            } else if (i % 3 == 0) {
+                result.add("Fizz");
+            } else if (i % 5 == 0) {
+                result.add("Buzz");
+            } else {
+                result.add("" + i);
+            }
+        }
+        return result;
+    }
+}
